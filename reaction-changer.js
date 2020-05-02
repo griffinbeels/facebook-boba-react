cfbcc59a6317813df32e30386e541f64072808ce
Griffin Beels <gbeels@Griffins-MacBook-Pro.local>
2020-05-01 18:37:40 -1000

    replaced ResizeSensor with clay.js; fixed scrolling to work (as far as I can tell) 100% of the time, might need more testing.

--- reaction-changer.js
+++ reaction-changer.js
@@ -2,15 +2,21 @@
  * Google Chrome extension to change the Facebook "Care" react png to a "Boba" react
  * underneath every post on Facebook's desktop site.
  *
- * By utilizing the css-element-queries (https://github.com/marcj/css-element-queries) 
- * library and jQuery, as well as analyzing Facebook's page structure, the reaction-changer.js
- * file allows us to replace any reaction emoji on Facebook.  This project was inspired by
+ * By utilizing the clay.js (https://github.com/zzarcon/clay) library for resizing and jQuery,
+ * as well as analyzing Facebook's page structure, this allows for the
+ * replacement of any reaction emoji on Facebook; in this specific case,
+ * we focus on the Care react and changing it to Boba.  This project was inspired by
  * a post on Subtle Asian Traits on Facebook (here: https://i.imgur.com/MGqFBUi.png).
+ * 
+ * Currently replaces Care react with Boba react for: 
+ * - Any post after first opening Facebook
+ * - Any post that loads after the user scrolls down
  *
  * @link   https://github.com/griffinbeels/facebook-boba-react
  * @author Griffin Beels, Conrad Zborowski.
  * @since  5/1/2020
  */
+
 // on DOM load, watch for future feed scrolling
 document.addEventListener('DOMContentLoaded', checkFeedUpdate(), false);
 
@@ -19,20 +25,16 @@ document.addEventListener('DOMContentLoaded', checkFeedUpdate(), false);
  * (i.e., changed in height), and if so, replaces any new 
  * Care react icons with Boba react icons.
  * 
- * By using ResizeSensor from css-element-queries, whenever the 
- * Facebook feed loads more posts, all Care react icons will 
- * be updated.
+ * By using clay.js' resize whenever the Facebook feed loads more posts,
+ * clay.js will detect a resize of the feed, and all Care react icons will be updated.
  */
 function checkFeedUpdate(){
-    // make sure we call this at least once even w/o resizes (might be unnecessary)
-    replaceCareWithBoba();
+    // id #contentArea represents the facebook feed
+    var fbFeed = new Clay("#contentArea");
 
-    // id #contentArea represents the facebook feed that scrolls
-    // NOTE: for whatever reason, you can't simply pass 
-    // a function into ResizeSensor; instead, you need to 
-    // create an anonymous function, and inside that, call whatever
-    // method is needed.
-    new ResizeSensor($("#contentArea"), function(){
+    // resize occurs whenever the user scrolls down or a comment loads
+    // on a prexisting post
+    fbFeed.on('resize', function() {
         replaceCareWithBoba()
     });
 }
